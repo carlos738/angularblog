d242efe1dde11d89bf04f59576b9b17867d6be7a
carlos738 <carlosasl135@hotmail.com>
2022-11-26 19:00:58 -0300

    dynamic

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake =[
+  {
+    "id": 1,
+    "photo":"aaa",
+    "title":"saiu novo filme",
+    "description":"ola mundo!",
+  },
+  {
+    "id": 2,
+    "photo":"aaa",
+    "title":"saiu novo filme",
+    "description":"ola mundo!",
+  }
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  photoCover:string="https://i.pinimg.com/736x/aa/2f/cd/aa2fcd7f3b58361ab818c085b4e37da5.jpg"
+  contentTitle:string=""
+  contentDescription:string=""
 
   constructor() { }
 
